42c3e2eae664f0b1dc370dcfaefcc55b4efbec8a
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-05 19:57:11 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,4 +18,11 @@ function whileLoop(number){
   return 'done'
 }
 
-function doWhileLoop(array)
+
+function doWhileLoop(array){
+  do {
+    array - 1 
+} while { function maybeTrue() {
+  return Math.random() >= 0.5
+}
+}
